d563c48e08b34c542e4fbc28d2304bab11bb4cd8
Enock Jeremi <enockjeremi@gmail.com>
2024-02-24 23:19:43 -0300

    feat(navbar): added ability to create new workspaces
    - Implemented functionality to create new workspaces in the navbar.
    - Added a form with an input field and a button.
    - Aesthetics are completed.
    - Utilized react-hook-form for data input.
    - Error handling implemented using Joi.

--- src/app/(pages)/(home)/components/sidebar-component.tsx
+++ src/app/(pages)/(home)/components/sidebar-component.tsx
@@ -7,6 +7,10 @@ import { ListDetailsIcon } from "@/app/client/components/icons/list-details-icon
 import { NavbarCollapseIcon } from "@/app/client/components/icons/navbar-collapse-icon";
 import { NavbarExpandIcon } from "@/app/client/components/icons/navbar-expand-icon";
 import { StackIcon } from "@/app/client/components/icons/stack-icon";
+import { PlusIcon } from "@/app/client/components/icons/plus-icon";
+import { SubmitHandler, useForm } from "react-hook-form";
+import Joi from "joi";
+import { joiResolver } from "@hookform/resolvers/joi";
 
 const workSpaces = [
   { id: 1, name: "Espacio de trabajo 1" },
@@ -14,8 +18,27 @@ const workSpaces = [
   { id: 3, name: "Espacio de trabajo 3" },
 ];
 
+interface IWorkspaces {
+  name: string;
+}
+const schema = Joi.object({
+  name: Joi.string().min(4).required(),
+});
+
 const SidebarComponent = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IWorkspaces>({ resolver: joiResolver(schema) });
   const [toggle, setToogle] = useState<boolean>(false);
+  const [toggleInputAddWorkspaces, setToggleInputAddWorkspaces] =
+    useState<boolean>(false);
+
+  const onSubmit: SubmitHandler<IWorkspaces> = (data) => {
+    console.log(data);
+  };
+  
   return (
     <nav className="w-full text-white">
       <button
@@ -46,9 +69,33 @@ const SidebarComponent = () => {
             <li className="text-md py-2 my-4 px-3 border-y border-white border-dashed space-x-3 flex items-center">
               <span>Projectos en desarrollo</span>
               <CopyPlusIcon
-                onClick={() => console.log("add workSpaces")}
+                onClick={() =>
+                  setToggleInputAddWorkspaces(!toggleInputAddWorkspaces)
+                }
                 className="w-5 cursor-pointer"
               />
+            </li>
+            <li>
+              <form
+                className={`pb-4 ${
+                  toggleInputAddWorkspaces ? "hidden" : "flex"
+                } duration-150 transition-all`}
+                onSubmit={handleSubmit(onSubmit)}
+              >
+                <input
+                  {...register("name")}
+                  className={`text-dark ${
+                    errors.name ? "bg-red-300" : "bg-white"
+                  } rounded-y-md rounded-l-md w-full  px-1 py-2 focus:outline-none`}
+                />
+                <button
+                  className={`rounded-md rounded-l-none ${
+                    errors.name ? "bg-red-500" : "bg-primary"
+                  } bg-primary py-2 text-white duration-100 hover:bg-extradark hover:text-white`}
+                >
+                  <PlusIcon />
+                </button>
+              </form>
             </li>
             {workSpaces.map((item) => (
               <li
