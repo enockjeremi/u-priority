4e06077cd8090e199d2fe6038e87da680c7ce328
Enock Jeremi <enockjeremi@gmail.com>
2024-03-06 23:40:27 -0300

    feat(ui): reverted to original idea for task editing
    - Removed the library for handling double clicks.
    - Introduced a separate button for task editing, transitioning to a form view.

--- src/app/(pages)/(home)/spaces/[id]/components/side-info-tasks-component.tsx
+++ src/app/(pages)/(home)/spaces/[id]/components/side-info-tasks-component.tsx
@@ -1,8 +1,23 @@
 "use client";
+import { TODO } from "@/types/todo.types";
 import { ITask } from "@/types/workspaces";
-import { Chip, Input, Textarea, Typography } from "@material-tailwind/react";
-import React, { FormEvent, useState } from "react";
+import {
+  Chip,
+  Input,
+  Select,
+  Textarea,
+  Typography,
+  Option,
+} from "@material-tailwind/react";
+import React, { useState } from "react";
 import { useDoubleTap } from "use-double-tap";
+
+const status = [
+  { id: 1, status: "Completado" },
+  { id: 2, status: "En desarrollo" },
+  { id: 3, status: "Pendiente" },
+  { id: 4, status: "Pausado" },
+];
 
 const SideInfoTasksComponent = ({
   tasksInfo,
@@ -23,51 +38,33 @@ const SideInfoTasksComponent = ({
     },
   });
 
-  const [inputEdit, setInputEdit] = useState(false);
-  const handleDoubleClick = useDoubleTap(() => {
-    setInputEdit(true);
-  });
-  const handleBlur = () => setInputEdit(false);
-
-  const handleChange = (e: any) => {
-    e.preventDefault();
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value,
-    });
-  };
 
   return (
     <>
       <div className="w-full p-6 mt-10 flex flex-col gap-4">
-        {inputEdit ? (
-          <Input
-            crossOrigin={undefined}
-            onBlur={handleBlur}
-            onChange={handleChange}
-            defaultValue={tasksInfo?.name || ""}
-            color="white"
-            label="Nombre de la tarea"
-            autoFocus
-          />
-        ) : (
-          <Typography
-            placeholder={undefined}
-            variant="h3"
-            color="white"
-            {...handleDoubleClick}
-            className="font-normal leading-none"
-          >
-            {tasksInfo?.name || ""}
-          </Typography>
-        )}
+        
+        <Typography
+          placeholder={undefined}
+          variant="h3"
+          color="white"
+          className="font-normal leading-none"
+        >
+          {tasksInfo?.name || ""}
+        </Typography>
+
         <div className="w-full">
-          <div className="min-h-[100px] pt-5 text-white">
+          <div
+            className="min-h-[100px] pt-5 text-white"
+          >
             {tasksInfo?.description}
           </div>
         </div>
+        
         <div className="flex justify-between items-center">
-          <Chip size="md" value={tasksInfo?.status.status || ""} />
+          <Chip
+            size="md"
+            value={tasksInfo?.status.status || ""}
+          />
           <Chip
             size="md"
             value={tasksInfo?.priority.priority || ""}
